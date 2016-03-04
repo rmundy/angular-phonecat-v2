a37d04f819aa0e6278ddaddd2a3e74721ccb4805
Bobby Mundy <robert.l.mundy@gmail.com>
2016-03-03 21:02:29 -0500

    Finished Step3

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -13,6 +13,4 @@ phonecatApp.controller('PhoneListCtrl', function($scope) {
       'name': 'MOTOROLA XOOM™',
       'snippet': 'The Next, Next Generation tablet.'
     }];
-
-    $scope.name = "World";
 });
